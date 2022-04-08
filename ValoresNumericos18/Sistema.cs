c2c39fea9c0da5bc1b076aa0522c8f5231c43b72
JoelCAI <joelargentina12345@gmail.com>
2022-04-08 14:42:43 -0300

    Agregar archivos de proyecto.

---
+++ ValoresNumericos18/Sistema.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValoresNumericos18
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+            decimal numeroUno;
+            decimal numeroDos;
+
+
+
+            Validador.Bienvenida();
+
+            numeroUno = Validador.PedirNumeroDecimal("\n Ingrese el primero valor numérico.");
+
+            Console.Clear();
+            
+            
+            decimal numeroFinal = decimal.Round(numeroUno, 2);
+
+            Console.Clear();
+            Console.WriteLine(" \n Usted Ingreso el numero: *" + numeroUno + "*. \n El número ingresado a 2 decimales es:" +
+                               + numeroFinal );
+            
+            Console.WriteLine(" \n Si el resultado no fue el esperado es porque ignoró la recomendación e ingreso" +
+                              " un punto en vez de una coma");
+
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
